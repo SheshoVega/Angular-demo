cdaf4219da866715254865d4d07a1071627eaf04
SheshoVega <hvega.sergio@gmail.com>
2018-01-25 19:27:41 -0600

    agregando project details component

---
+++ src/app/projects/projects-details/projects-details.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import { HttpClient } from "@angular/common/http";
+
+//RXJS operators
+import 'rxjs/add/operator/map';
+
+import { Project } from "../../core/models/project";
+import { ProjectsService } from '../projects.service';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Component({
+	selector: 'app-projects-details',
+	templateUrl: './projects-details.component.html',
+	styleUrls: ['./projects-details.component.scss']
+})
+export class ProjectsDetailsComponent implements OnInit, OnDestroy {
+
+	paramsSub: Subscription;
+	project: Project;
+
+	constructor(private route: ActivatedRoute, private projectsService: ProjectsService) { }
+
+	ngOnInit() {
+
+		this.paramsSub = this.route.params.map(params => params['id']).subscribe(projectId => {
+			this.projectsService.getProject(projectId).subscribe(
+				(data: Project) => {
+					console.log(data);
+					this.project = data;
+				},
+				(err: HttpErrorResponse) => {
+					if (err.error instanceof Error) {
+						console.log('Client-Side error:', err.error.message);
+					} else {
+						console.log(`Backend error: codigo [ ${err.status} ], error: [ ${err.error} ]`);
+					}
+				}
+			);
+		});
+
+	}
+
+	ngOnDestroy() {
+		this.paramsSub.unsubscribe();
+	}
+
+}
