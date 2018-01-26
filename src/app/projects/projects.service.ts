1ebd8203ac76ae052bc19803bdf33abf5cf2ffc6
SheshoVega <hvega.sergio@gmail.com>
2018-01-25 19:19:49 -0600

    agregando projects service

---
+++ src/app/projects/projects.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+import { Project } from '../core/models/project';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+@Injectable()
+export class ProjectsService {
+
+	private apiRoot: string = 'http://localhost:3000/projects';
+	operationResultSource = new BehaviorSubject<boolean>(false);
+	operationResultObservable = this.operationResultSource.asObservable();
+
+	constructor(private http: HttpClient) { }
+
+	setOperationResult(result:boolean){
+		this.operationResultSource.next(result);
+	}
+
+	getOperationResult(): Observable<boolean>{
+		return this.operationResultObservable;
+	}
+
+	getProjects(): Observable<Project[]> {
+		return this.http.get<Project[]>(this.apiRoot);
+	}
+
+	getProject(id:string):Observable<Project> {
+		let apiUrl = `${this.apiRoot}/${id}`;
+		return this.http.get<Project>(apiUrl);
+	}
+
+	deleteProject(id:string):Observable<any>{
+		let apiUrl = `${this.apiRoot}/${id}`;
+		return this.http.delete(apiUrl);
+	}
+
+	createProject(project:Project){
+		return this.http.put(this.apiRoot,project);
+	}
+
+	updateProject(project:Project){
+		return this.http.post(this.apiRoot,project);
+	}
+}
