6d6abf5c720356d156b4cb6a0bc52f67a348634c
SheshoVega <hvega.sergio@gmail.com>
2018-01-25 19:28:08 -0600

    actualizando app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { SharedModule } from "./shared/shared.module";
+import { ProjectsModule } from "./projects/projects.module";
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './core/dashboard/dashboard.component';
+import { HomeComponent } from './core/home/home.component';
+
+import { routes } from "./app.routes";
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		DashboardComponent,
+		HomeComponent
+	],
+	imports: [
+		BrowserModule,
+		RouterModule.forRoot(routes),
+		SharedModule,
+		ProjectsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
